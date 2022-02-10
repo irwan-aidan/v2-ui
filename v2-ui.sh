176e27501530aeae8f9cde69b346bdf0995540b0
irwan-aidan <91520148+irwan-aidan@users.noreply.github.com>
2022-02-10 16:58:14 +0800

    Update v2-ui.sh

--- v2-ui.sh
+++ v2-ui.sh
@@ -90,7 +90,7 @@ before_show_menu() {
 }
 
 install() {
-    bash <(curl -Ls https://raw.githubusercontent.com/doublog/v2-ui/master/v2-ui.sh)
+    bash <(curl -Ls https://raw.githubusercontent.com/wixfreto/v2-ui/master/v2-ui.sh)
     if [[ $? == 0 ]]; then
         if [[ $# == 0 ]]; then
             start
@@ -109,7 +109,7 @@ update() {
         fi
         return 0
     fi
-    bash <(curl -Ls https://raw.githubusercontent.com/doublog/v2-ui/master/v2-ui.sh)
+    bash <(curl -Ls https://raw.githubusercontent.com/wixfreto/v2-ui/master/v2-ui.sh)
     if [[ $? == 0 ]]; then
         echo -e "${green}更新完成，已自动重启面板${plain}"
         exit
@@ -285,7 +285,7 @@ show_log() {
 }
 
 install_bbr() {
-    bash <(curl -L -s https://raw.githubusercontent.com/sprov065/blog/master/bbr.sh)
+    bash <(curl -L -s https://raw.githubusercontent.com/wixfreto/blog/master/bbr.sh)
     if [[ $? == 0 ]]; then
         echo ""
         echo -e "${green}安装 bbr 成功${plain}"
@@ -298,7 +298,7 @@ install_bbr() {
 }
 
 update_shell() {
-    wget -O /usr/bin/v2-ui -N --no-check-certificate https://github.com/doublog/v2-ui/raw/master/v2-ui.sh
+    wget -O /usr/bin/v2-ui -N --no-check-certificate https://github.com/wixfreto/v2-ui/raw/master/v2-ui.sh
     if [[ $? != 0 ]]; then
         echo ""
         echo -e "${red}下载脚本失败，请检查本机能否连接 Github${plain}"
